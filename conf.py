0e532ff669ba1c4d99a7c3149d793a08acf995bb
Evg <mishagin02@mail.ru>
2023-09-01 21:01:15 +0300

    change conf

--- conf.py
+++ conf.py
@@ -10,6 +10,7 @@ project = 'test'
 copyright = '2023, Evgeny198'
 author = 'Evgeny198'
 release = '0.0.1'
+source_dir = '.'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
